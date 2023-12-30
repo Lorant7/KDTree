afdf8ae235b366f56912ca0991fbf2399caebc61
Lorant7 <tomy.lorant@gmail.com>
2023-12-30 18:10:43 -0500

    implemented KD-Tree struct and can now add nodes to the tree

--- main.c
+++ main.c
@@ -10,8 +10,8 @@ typedef struct KDNode
     int point[K];
 
     // Pointers to the left and right nodes of the node in the tree
-    struct Node *left;
-    struct Node *right;
+    struct KDNode *left;
+    struct KDNode *right;
 } Node;
 
 // Node initializer (this does not add it to the tree, it simply creates the new node)
@@ -33,18 +33,22 @@ Node *newNode(int *data)
 // Data strucutre for the kd tree
 // for now it just holds the root of the tree (might want to add other vairables such as
 // the cound of nodes on each side to check if it balanced, etc)
-// typedef struct kdTree
-// {
-//     Node *root;
-// } Tree;
+typedef struct kdTree
+{
+    Node *root;
+    int nodeCount;
 
-// Tree *newTree(void)
-// {
-//     Tree *nTree = (Tree *)malloc(sizeof(Tree));
-//     nTree->root = NULL;
+} Tree;
 
-//     return nTree;
-// }
+Tree *newTree(void)
+{
+    Tree *nTree = (Tree *)malloc(sizeof(Tree));
+    nTree->root = NULL;
+
+    nTree->nodeCount = 0;
+
+    return nTree;
+}
 
 // Remove a node from the tree
 // TODO: need to find a heuristic that when a node is removed the tree continues to be balanced
@@ -58,61 +62,101 @@ void removeNode(Node *node)
 // Function that adds the given node to the tree
 // TODO: figure out if you can add nodes in a way such that the tree stays balanced
 // (I don't think so but, there might be a better way to do it instead of doing it like a simple binar tree.)
-// Node *addNode(Node *newNode, Tree *tree)
-// {
+Node *addNode(Node *newNode, Tree *tree)
+{
 
-// int i = 0;
-// int found = 0;
-// Node *current = tree->root;
+    int i = 0;
+    int found = 0;
+    int left = 1;
+    Node *current = tree->root;
+    Node *prev = NULL;
 
-// // You can probably generalize these if statments with the array you use
-// while (!found)
-// {
-//     // TODO: there is no root (OR WE FOUND WHERE TO PLACE IT), make the given node the root
-//     if (current == NULL)
-//     {
-//     }
-//     // if the number is even check x, if the number is odd check y
-//     if (i % 2 == 0)
-//     {
-//         if (newNode->point[0] < current->point[0])
-//         {
-//             current = current->left;
-//         }
-//         else
-//         {
-//             current = current->right;
-//         }
-//     }
-//     else
-//     {
-//         if (newNode->point[1] < current->point[1])
-//         {
-//             current = current->left;
-//         }
-//         else
-//         {
-//             current = current->right;
-//         }
-//     }
-// }
-// }
+    // You can probably generalize these if statments with the array you use
+    while (!found)
+    {
+        // TODO: there is no root (OR WE FOUND WHERE TO PLACE IT), make the given node the root
+        if (current == NULL)
+        {
+            found = 1;
+            if (prev == NULL)
+            {
+                tree->root = newNode;
+            }
+            else
+            {
+
+                if (left)
+                {
+                    prev->left = newNode;
+                }
+                else
+                {
+                    prev->right = newNode;
+                }
+            }
+        }
+        else
+        {
+            // if the number is even check x, if the number is odd check y
+            prev = current;
+            if (i % 2 == 0)
+            {
+                if (newNode->point[0] < current->point[0])
+                {
+                    current = current->left;
+                    left = 1;
+                }
+                else
+                {
+                    current = current->right;
+                    left = 0;
+                }
+            }
+            else
+            {
+                if (newNode->point[1] < current->point[1])
+                {
+                    current = current->left;
+                    left = 1;
+                }
+                else
+                {
+                    current = current->right;
+                    left = 0;
+                }
+            }
+            i = i + 1;
+        }
+    }
+
+    tree->nodeCount = tree->nodeCount + 1;
+}
 
 int main()
 {
+
     printf("Hello, World!\n");
+
+    Tree *tree = newTree();
+    printf("Node count: %d\n", tree->nodeCount);
     int arr[2];
     arr[0] = 1;
     arr[1] = 2;
     Node *node1 = newNode(arr);
-    printf("x: %d y: %d \n", node1->point[0], node1->point[1]);
+    printf("node 1 values:  x: %d y: %d \n", node1->point[0], node1->point[1]);
+
+    addNode(node1, tree);
+    printf("Node count: %d\n", tree->nodeCount);
 
     arr[0] = 3;
     arr[1] = 4;
 
     Node *node2 = newNode(arr);
-    printf("x: %d y: %d \n", node2->point[0], node2->point[1]);
+    printf("node 2 values:  x: %d y: %d \n", node2->point[0], node2->point[1]);
 
-    removeNode(node1);
-    removeNode(node2);
+    addNode(node2, tree);
+    printf("Node count: %d\n", tree->nodeCount);
+
+    // removeNode(node1);
+    // removeNode(node2);
 };
